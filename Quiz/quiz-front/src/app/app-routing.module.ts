20574f2cb9bfed25b5895463b956aa00d1995ede
Nurzhigit Smailov <nusmailov@gmail.com>
2019-04-30 01:35:18 +0600

    mainComponent

--- Quiz/quiz-front/src/app/app-routing.module.ts
+++ Quiz/quiz-front/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {ProviderService} from './shared/services/provider.service';
 
-const routes: Routes = [];
+import {MainComponent} from './main/main.component';
+
+const routes: Routes = [
+  {path: '', component: MainComponent}
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Quiz/quiz-front/src/app/app.module.ts
+++ Quiz/quiz-front/src/app/app.module.ts
@@ -1,18 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {ClassProvider,  NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
+import {FormsModule} from '@angular/forms';
+import {ProviderService} from './shared/services/provider.service';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
   ],
-  providers: [],
+  providers: [
+    ProviderService,
+    ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
